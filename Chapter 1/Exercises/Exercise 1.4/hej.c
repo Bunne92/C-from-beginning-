96d4dd99e6bf6a2c8db00f98836c2bf1e9365fe0
Dennis Bunne <dennisbunne92@gmail.com>
2017-12-23 12:52:29 +0100

    Exercise 1.7 code

--- Chapter 1/Exercises/Exercise 1.4/hej.c
+++ Chapter 1/Exercises/Exercise 1.4/hej.c
@@ -1,3 +1,12 @@
+/*
+ *File: hej.c
+ *Date: 2017-12-23
+ *Author: Dennis Bunne
+ *
+ *Exercise 1.4 from the swedish book "C från början"
+ *A program who prints "hej"
+ */
+
 #include <stdio.h>
 
 int main(){
---
+++ Chapter 1/Exercises/Exercise 1.7/name.c
@@ -0,0 +1,16 @@
+/*
+ *File: name.c
+ *Date: 2017-12-23
+ *Author: Dennis Bunne
+ *
+ *Exercise 1.7 from the swedish book "C från början"
+ *A program who prints out your name and phone-number.
+ */
+
+#include <stdio.h>
+
+int main(){
+  printf("Dennis Bunne\n"
+         "012-3456 789");
+  return 0;
+}
