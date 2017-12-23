15b6cff8a37982520688191ee6c4e3517e8a1fec
Dennis Bunne <dennisbunne92@gmail.com>
2017-12-23 13:13:38 +0100

    Assignment 1 code

---
+++ Chapter 1/Assignments/Assignment 1.1/name.c
@@ -0,0 +1,16 @@
+/*
+ *File: name.c
+ *Date: 2017-12-23
+ *Author: Dennis Bunne
+ *
+ *Assignment 1.1 from the swedish book "C från början"
+ *A program who prints out your name and phone-number.
+ */
+
+#include <stdio.h>
+
+int main(){
+  puts("Dennis Bunne");           //Using puts instead of printf
+  puts("012-3456 789");
+  return 0;
+}
