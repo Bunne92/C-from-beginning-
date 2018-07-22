b7e94b78d02ac41dccd674ffeaca7ffbe92274ca
Dennis Bunne <dennisbunne92@gmail.com>
2018-07-22 19:13:17 +0200

    Assignment 2.1 code

---
+++ Chapter 2/Assignments/Assignment 2.1/main.c
@@ -0,0 +1,29 @@
+/*
+ *File: main.c
+ *Date: 2018-07-22
+ *Author: Dennis Bunne
+ *
+ *Assignment 2.1 from the swedish book "C från början"
+ *
+ *
+ */
+#include <stdio.h>
+#include <math.h>
+
+
+#define PI 3.1415
+
+int main(){
+	int r, area, volume;
+
+	printf("Enter radie of the sphere: ");
+	scanf("%d", &r);
+
+	volume = 4*PI*r*r*r/3; 
+	area = 4*PI*r*r;
+
+	printf("Volume of the sphere: %d\n", volume);
+	printf("Area of the sphere: %d\n", area);
+	return 0;
+
+}
