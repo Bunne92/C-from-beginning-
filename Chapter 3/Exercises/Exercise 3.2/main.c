23e98310cf79ee8a9272bb1e329df322619d59d7
Dennis Bunne <dennisbunne92@gmail.com>
2018-07-28 12:07:52 +0200

    Exercise 3.2 code

--- Chapter 3/Exercises/Exercise 3.2/main.c
+++ Chapter 3/Exercises/Exercise 3.2/main.c
@@ -8,3 +8,25 @@
  *
  */
 #include <stdio.h>
+
+int main(){
+	int priceYear, priceTicket, times, sum;	
+
+	printf("Enter price for season ticket: ");
+	scanf("%d",&priceYear);
+
+	printf("Enter price for a ticket: ");
+	scanf("%d", &priceTicket);
+
+	printf("Enter how many times u will visit the gym: ");
+	scanf("%d", &times);
+
+	if(priceTicket*times > priceYear){
+		printf("Choose season ticket.");
+	}
+	else{
+		printf("Choose one-time tickets.");
+	}
+
+	return 0;
+}
